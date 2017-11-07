d8e88a96d1a46f50c47afe77f55bba3c0c1d254e
Tomasz Szyborski <tomasz.szyborski@gmail.com>
2017-11-07 10:34:08 +0100

    added my_answers.py

--- 2nd_Semester/time_series_prediction_and_text_generation/my_answers.py
+++ 2nd_Semester/time_series_prediction_and_text_generation/my_answers.py
@@ -23,14 +23,19 @@ def window_transform_series(series, window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
+    model = Sequential()
+    model.add(LSTM(5, input_shape=(window_size, 1)))
+    model.add(Dense(1))
+
+    return model
 
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
+    allowed = string.ascii_lowercase + ''.join(punctuation)
+    return ''.join([char if char in allowed else " " for char in text])
 
-    return text
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_text(text, window_size, step_size):
@@ -38,9 +43,18 @@ def window_transform_text(text, window_size, step_size):
     inputs = []
     outputs = []
 
-    return inputs,outputs
+    for i in range(0, len(text) - window_size, step_size):
+        inputs.append(text[i : i + window_size])
+        outputs.append(text[i + window_size])
+
+    return inputs, outputs
 
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
-    pass
+    model = Sequential()
+    model.add(LSTM(200, input_shape=(window_size, num_chars)))
+    model.add(Dense(num_chars))
+    model.add(Activation('softmax'))
+
+    return model
